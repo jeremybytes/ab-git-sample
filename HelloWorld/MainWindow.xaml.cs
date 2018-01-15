4519c3a43560f217992492bf9d1db2837db5049b
Jeremy Clark <jeremy@jeremybytes.com>
2018-01-15 11:02:20 +0000

    Added event handler

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -8,6 +8,12 @@ namespace HelloWorld
         public MainWindow()
         {
             InitializeComponent();
+            ClickMeButton.Click += ClickMeButton_Click;
+        }
+
+        private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
         }
     }
 }
